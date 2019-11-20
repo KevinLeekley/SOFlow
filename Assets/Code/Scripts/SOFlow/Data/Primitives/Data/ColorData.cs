7199d22741195708c78ac185ebfc3803c720c027
Kearan Petersen <kearan.petersen@purple-muffin.com>
2019-11-20 10:49:26 +0800

    Added the Color primitive data type to the SDK.

---
+++ Assets/Code/Scripts/SOFlow/Data/Primitives/Data/ColorData.cs
@@ -0,0 +1,264 @@
+﻿// Created by Kearan Petersen : https://www.blumalice.wordpress.com | https://www.linkedin.com/in/kearan-petersen/
+
+using SOFlow.Data.Events;
+using SOFlow.Utilities;
+using UnityEngine;
+
+namespace SOFlow.Data.Primitives
+{
+    [CreateAssetMenu(menuName = "SOFlow/Data/Primitives/Color")]
+    public class ColorData : PrimitiveData
+    {
+        /// <summary>
+        ///     Event raised when this primitive data has changed.
+        /// </summary>
+        public DynamicEvent OnDataChanged;
+
+        /// <summary>
+        ///     Event raised when an update occurs on this primitive data.
+        ///     The data does not necessarily change when this event is called.
+        /// </summary>
+        public DynamicEvent OnDataUpdated;
+
+        /// <summary>
+        ///     Determines whether the true asset value should retain
+        ///     value changes during Play Mode.
+        /// </summary>
+        public bool PersistInPlayMode;
+
+        /// <summary>
+        ///     The true asset value of this data.
+        /// </summary>
+        public Color AssetValue;
+        
+        /// <summary>
+        ///     The Play Mode safe representation of this data.
+        /// </summary>
+        [SerializeField]
+        protected Color _playModeValue;
+
+        /// <summary>
+        ///     The value for this data.
+        /// </summary>
+        public Color Value
+        {
+            get
+            {
+#if UNITY_EDITOR
+                // Return the Play Mode safe representation of the data during
+                // Play Mode.
+                if(Application.isPlaying) return _playModeValue;
+#endif
+                // Always return the true asset value during Edit Mode.
+                return AssetValue;
+            }
+            set
+            {
+#if UNITY_EDITOR
+                if(Application.isPlaying)
+                {
+                    // Only alter the Play Mode safe representation of
+                    // this data during Play Mode.
+                    if(!_playModeValue.Equals(value))
+                    {
+                        _playModeValue = value;
+
+                        OnDataChanged.Invoke(new SOFlowDynamic
+                                             {
+                                                 Value = GetValue()
+                                             });
+
+                        if(PersistInPlayMode)
+                            // If desired, the true asset value can maintain
+                            // the changes created during Play Mode.
+                            AssetValue = value;
+                    }
+                }
+                else
+                {
+                    if(!AssetValue.Equals(value))
+                    {
+                        AssetValue = value;
+
+                        OnDataChanged.Invoke(new SOFlowDynamic
+                                             {
+                                                 Value = GetValue()
+                                             });
+                    }
+                }
+#else
+                if(!AssetValue.Equals(value))
+                {
+                    AssetValue = value;
+                    
+                    OnDataChanged.Invoke(new SOFlowDynamic
+                                         {
+                                             Value = GetValue()
+                                         });
+                }
+#endif
+                OnDataUpdated.Invoke(new SOFlowDynamic
+                                     {
+                                         Value = GetValue()
+                                     });
+            }
+        }
+
+        /// <summary>
+        ///     Returns the value of this data.
+        /// </summary>
+        /// <returns></returns>
+        public Color GetValue()
+        {
+            return Value;
+        }
+
+        /// <inheritdoc />
+        public override object GetValueData()
+        {
+            return Value;
+        }
+
+#if UNITY_EDITOR
+        protected void OnValidate()
+        {
+            // Resync the Play Mode safe representation with the
+            // true asset value during editing.
+            _playModeValue = AssetValue;
+        }
+#endif
+
+        /// <summary>
+        ///     Attempts to set the value of this data to the supplied value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetValue(SOFlowDynamic value)
+        {
+            if(value.Value is Color)
+                Value = (Color)value.Value;
+            else if(value.Value is ColorData)
+                Value = ((ColorData)value.Value).Value;
+            else
+                Debug.LogWarning($"[ColorData] Supplied value is not a supported data type.\n{name}");
+        }
+
+        /// <summary>
+        ///     Attempts to set the value of this data to the supplied value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetValue(ColorData value)
+        {
+            Value = value.Value;
+        }
+
+        #region Base Modifiers
+
+        /// <summary>
+        ///     Adds an amount to the data value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddTo(Color value)
+        {
+            Value += value;
+        }
+
+        /// <summary>
+        ///     Subtracts an amount from the data value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void SubtractFrom(Color value)
+        {
+            Value -= value;
+        }
+
+        /// <summary>
+        ///     Multiplies an amount to the data value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void MultiplyWith(Color value)
+        {
+            Value *= value;
+        }
+
+        #endregion
+
+        #region Data Modifiers
+
+        /// <summary>
+        ///     Adds an amount to the data value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddTo(ColorData value)
+        {
+            Value += value.Value;
+        }
+
+        /// <summary>
+        ///     Subtracts an amount from the data value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void SubtractFrom(ColorData value)
+        {
+            Value -= value.Value;
+        }
+
+        /// <summary>
+        ///     Multiplies an amount to the data value.
+        /// </summary>
+        /// <param name="value"></param>
+        public void MultiplyWith(ColorData value)
+        {
+            Value *= value.Value;
+        }
+
+        #endregion
+
+        #region Base Comparisons
+
+        /// <summary>
+        ///     Checks if the data value is equal to the given value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Equal(Color value)
+        {
+            return Value.Equals(value);
+        }
+
+        /// <summary>
+        ///     Checks if the data value is not equal to the given value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool NotEqual(Color value)
+        {
+            return !Value.Equals(value);
+        }
+
+        #endregion
+
+        #region Data Comparisons
+
+        /// <summary>
+        ///     Checks if the data value is equal to the given value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Equal(ColorData value)
+        {
+            return Value.Equals(value.Value);
+        }
+
+        /// <summary>
+        ///     Checks if the data value is not equal to the given value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool NotEqual(ColorData value)
+        {
+            return !Value.Equals(value.Value);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Code/Scripts/SOFlow/Data/Primitives/Fields/ColorField.cs
@@ -0,0 +1,90 @@
+﻿// Created by Kearan Petersen : https://www.blumalice.wordpress.com | https://www.linkedin.com/in/kearan-petersen/
+
+using System;
+using System.Globalization;
+using UnityEngine;
+
+namespace SOFlow.Data.Primitives
+{
+    [Serializable]
+    public class ColorField : DataField
+    {
+        /// <summary>
+        ///     The explicitly inferred type for the ConstantValue property.
+        /// </summary>
+        [HideInInspector]
+        public Color ConstantValueType;
+
+        /// <summary>
+        ///     The explicitly inferred type for the Variable property.
+        /// </summary>
+        [HideInInspector]
+        public ColorData VariableType;
+
+        /// <summary>
+        ///     The value of this field.
+        /// </summary>
+        public Color Value
+        {
+            get
+            {
+                if(UseConstant) return ConstantValue;
+
+                if(Variable == null) return ConstantValue;
+
+                return Variable.Value;
+            }
+            set
+            {
+                if(UseConstant)
+                {
+                    ConstantValue = value;
+                }
+                else
+                {
+                    if(Variable != null) Variable.Value = value;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     The volatile data for this field.
+        /// </summary>
+        public Color ConstantValue
+        {
+            get => ConstantValueType;
+            set => ConstantValueType = value;
+        }
+
+        /// <summary>
+        ///     The non-volatile data for this field.
+        /// </summary>
+        public ColorData Variable
+        {
+            get => VariableType;
+            set => VariableType = value;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        /// <summary>
+        ///     Implicit conversion to the corresponding data type.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static implicit operator Color(ColorField data)
+        {
+            return data.Value;
+        }
+
+        /// <inheritdoc />
+        public override PrimitiveData GetVariable()
+        {
+            return Variable;
+        }
+    }
+}
