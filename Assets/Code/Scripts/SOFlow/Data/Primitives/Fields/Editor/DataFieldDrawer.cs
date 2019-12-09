559423cf1c92dcdc1d24d9d17679ca5405d6c06f
Kearan Petersen <kearan.petersen@gmail.com>
2019-12-10 00:55:57 +0800

    Added functionality for dragging and dropping primitive data assets directly into primitive fields.

--- Assets/Code/Scripts/SOFlow/Data/Primitives/Fields/Editor/DataFieldDrawer.cs
+++ Assets/Code/Scripts/SOFlow/Data/Primitives/Fields/Editor/DataFieldDrawer.cs
@@ -1,9 +1,11 @@
 // Created by Kearan Petersen : https://www.blumalice.wordpress.com | https://www.linkedin.com/in/kearan-petersen/
 
 #if UNITY_EDITOR
+using Object = UnityEngine.Object;
 using System;
 using System.Reflection;
 using Pather.CSharp;
+using SOFlow.Extensions;
 using SOFlow.Internal;
 using UnityEditor;
 using UnityEngine;
@@ -16,6 +18,9 @@ namespace SOFlow.Data.Primitives.Editor
         private Rect  _currentPosition;
         private bool  _isConstant;
         private float _lineHeight;
+        private bool _updateDragReferences;
+        private bool _updateDragReferencePersistenceCheck;
+        private Object _dragReference;
 
         private float _positionWidth;
 
@@ -26,7 +31,7 @@ namespace SOFlow.Data.Primitives.Editor
         {
             DrawDataField(position, property, label);
         }
-
+        
         /// <summary>
         ///     Draws the data field.
         /// </summary>
@@ -42,8 +47,60 @@ namespace SOFlow.Data.Primitives.Editor
             else
             {
                 _useConstant = property.FindPropertyRelative("UseConstant");
-                _isConstant  = _useConstant.boolValue;
+                Event currentEvent = Event.current;
 
+                if(position.Contains(currentEvent.mousePosition))
+                {
+                    if(currentEvent.type == EventType.DragUpdated)
+                    {
+                        DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                        currentEvent.Use();
+                    }
+                    else if(currentEvent.type == EventType.DragExited && DragAndDrop.objectReferences.Length > 0)
+                    {
+                        Object             draggedObject     = DragAndDrop.objectReferences[0];
+                        SerializedProperty referenceProperty = property.FindPropertyRelative("VariableType");
+                        Type               variableType      = TypeExtensions.GetInstanceType(referenceProperty.type);
+
+                        if(draggedObject.GetType().IsAssignableFrom(variableType))
+                        {
+                            // For some reason, Unity is not saving the newly assigned property values if we save those values
+                            // here. The values stay alive for a few frames, then return to their previous values before the
+                            // change. I have been unable to figure out why this is the case. This is a temporary workaround
+                            // until a proper solution is made.
+                            _dragReference = draggedObject;
+                            _updateDragReferences = true;
+                        }
+
+                        currentEvent.Use();
+
+                        return;
+                    }
+                }
+                else if(_updateDragReferences)
+                {
+                    SerializedProperty referenceProperty = property.FindPropertyRelative("VariableType");
+                    referenceProperty.objectReferenceValue = _dragReference;
+                    _useConstant.boolValue = false;
+                    property.serializedObject.ApplyModifiedProperties();
+
+                    _updateDragReferences = false;
+                    _updateDragReferencePersistenceCheck = true;
+                }
+                else if(_updateDragReferencePersistenceCheck)
+                {
+                    SerializedProperty referenceProperty = property.FindPropertyRelative("VariableType");
+
+                    if(referenceProperty.objectReferenceValue != _dragReference || _useConstant.boolValue)
+                    {
+                        referenceProperty.objectReferenceValue = _dragReference;
+                        _useConstant.boolValue                 = false;
+                        property.serializedObject.ApplyModifiedProperties();
+                        _updateDragReferencePersistenceCheck = false;
+                    }
+                }
+
+                _isConstant             = _useConstant.boolValue;
                 label                   = EditorGUI.BeginProperty(position, label, property);
                 _currentPosition        = EditorGUI.PrefixLabel(position, label);
                 _positionWidth          = _currentPosition.width;
@@ -61,9 +118,9 @@ namespace SOFlow.Data.Primitives.Editor
                 {
                     _isConstant = false;
                 }
-                
+
                 buttonsRect.x += 25f;
-                
+
                 if(GUI.Button(buttonsRect, "V", _isConstant ? SOFlowStyles.PressedButton : SOFlowStyles.Button))
                 {
                     _isConstant = true;
