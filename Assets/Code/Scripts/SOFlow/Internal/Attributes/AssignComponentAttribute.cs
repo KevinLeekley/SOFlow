fb13ac033345c0e599917a6f305d2a0538a11f0b
Kearan Petersen <kearan.petersen@gmail.com>
2019-12-23 21:31:13 +0800

    Implemented the AssignComponent attribute and accompanying property drawer for allowing component fields to be auto assigned their values.

---
+++ Assets/Code/Scripts/SOFlow/Internal/Attributes/AssignComponentAttribute.cs
@@ -0,0 +1,25 @@
+﻿// Created by Kearan Petersen : https://www.blumalice.wordpress.com | https://www.linkedin.com/in/kearan-petersen/
+
+using System;
+using UnityEngine;
+
+namespace SOFlow.Internal
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public class AssignComponentAttribute : PropertyAttribute
+	{
+		/// <summary>
+		/// The type to assign to the component.
+		/// </summary>
+		public Type AssigningType = null;
+
+		public AssignComponentAttribute()
+		{
+		}
+
+		public AssignComponentAttribute(Type type)
+		{
+			AssigningType = type;
+		}
+	}
+}
